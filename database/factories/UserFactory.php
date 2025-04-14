6e33ecd5de3fc224cc4ed84396ed780564405bf2
Muhammad Hamidu <sani.muhammad38@gmail.com>
2025-04-14 12:00:49 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,11 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => 'Ussey Admin' ,
-            'email' => 'admin@usseytech.com.ng',
+         return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '@Ussey1058',
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
