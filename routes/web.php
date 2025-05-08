0032e52cdfabb5146df7f4613a991d05319c1045
Muhammad S. Hamidu <mhamidu@swglobal.local>
2025-05-08 12:27:39 +0100

    feat usseytech

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://usseytech.com.ng/auth/login');
 });
 
 
